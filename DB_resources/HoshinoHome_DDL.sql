aaaacf54f07275d4c66e936a903e57ad507ada55
Hwarang <keyein123@gmail.com>
2024-05-10 09:06:57 +0900

    DB SQL Complete

--- DB_resources/HoshinoHome_DDL.sql
+++ DB_resources/HoshinoHome_DDL.sql
@@ -14,16 +14,6 @@ CREATE TABLE `dong` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table `hoshinoHome`.`housedeal`
--- -----------------------------------------------------
-
-DROP TABLE IF EXISTS `housedeal`;
-
-CREATE TABLE `housedeal` (
-    `deal_code` bigint NOT NULL, `deal_amount` varchar(20) DEFAULT NULL, `deal_year` int DEFAULT NULL, `deal_month` int DEFAULT NULL, `deal_day` int DEFAULT NULL, `area` varchar(20) DEFAULT NULL, `floor` varchar(4) DEFAULT NULL, `cancel_deal_type` varchar(1) DEFAULT NULL, `apt_code` bigint DEFAULT NULL, PRIMARY KEY (`deal_code`), KEY `housedeal_apt_code_houseinfo_apt_code_fk_idx` (`apt_code`), CONSTRAINT `housedeal_apt_code_houseinfo_apt_code_fk` FOREIGN KEY (`apt_code`) REFERENCES `houseinfo` (`apt_code`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-
--- -----------------------------------------------------
 -- Table `hoshinoHome`.`houseinfo`
 -- -----------------------------------------------------
 
@@ -33,6 +23,16 @@ CREATE TABLE `houseinfo` (
     `apt_code` bigint NOT NULL, `build_year` int DEFAULT NULL, `road_name` varchar(40) DEFAULT NULL, `road_name_bonbun` varchar(5) DEFAULT NULL, `road_name_bubun` varchar(5) DEFAULT NULL, `road_name_seq` varchar(2) DEFAULT NULL, `road_name_basement_code` varchar(1) DEFAULT NULL, `road_name_code` varchar(7) DEFAULT NULL, `dong` varchar(40) DEFAULT NULL, `bonbun` varchar(4) DEFAULT NULL, `bubun` varchar(4) DEFAULT NULL, `si_gun_gu_code` varchar(5) DEFAULT NULL, `eub_myun_dong_code` varchar(5) DEFAULT NULL, `dong_code` varchar(10) DEFAULT NULL, `land_code` varchar(1) DEFAULT NULL, `apartment_name` varchar(40) DEFAULT NULL, `jibun` varchar(10) DEFAULT NULL, `lng` varchar(30) DEFAULT NULL, `lat` varchar(30) DEFAULT NULL, PRIMARY KEY (`apt_code`), UNIQUE KEY `UNIQUE` (
         `build_year`, `apartment_name`, `jibun`, `si_gun_gu_code`, `eub_myun_dong_code`
     ) /*!80000 INVISIBLE */, KEY `houseinfo_dong_code_dong_dong_code_fk_idx` (`dong_code`) /*!80000 INVISIBLE */, CONSTRAINT `houseinfo_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `hoshinoHome`.`housedeal`
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS `housedeal`;
+
+CREATE TABLE `housedeal` (
+    `deal_code` bigint NOT NULL, `deal_amount` varchar(20) DEFAULT NULL, `deal_year` int DEFAULT NULL, `deal_month` int DEFAULT NULL, `deal_day` int DEFAULT NULL, `area` varchar(20) DEFAULT NULL, `floor` varchar(4) DEFAULT NULL, `cancel_deal_type` varchar(1) DEFAULT NULL, `apt_code` bigint DEFAULT NULL, PRIMARY KEY (`deal_code`), KEY `housedeal_apt_code_houseinfo_apt_code_fk_idx` (`apt_code`), CONSTRAINT `housedeal_apt_code_houseinfo_apt_code_fk` FOREIGN KEY (`apt_code`) REFERENCES `houseinfo` (`apt_code`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
