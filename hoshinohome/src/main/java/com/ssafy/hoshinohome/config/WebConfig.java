65cc86fee0d3f42b1eaa7c6bbc2adb456b4a0ed6
ChanminLee <cmbear314@naver.com>
2024-05-12 00:39:10 +0900

    Fix: CORS 주소 추가

--- hoshinohome/src/main/java/com/ssafy/hoshinohome/config/WebConfig.java
+++ hoshinohome/src/main/java/com/ssafy/hoshinohome/config/WebConfig.java
@@ -39,7 +39,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:9090", "http://127.0.0.1:9000", "http://localhost:9000")
+                .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:9090", "http://127.0.0.1:9000",
+                        "http://localhost:9000", "http://localhost:5500")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
