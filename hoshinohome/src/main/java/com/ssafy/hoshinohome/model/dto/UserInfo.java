55db3b15f60d6a8172f2320d2020ce4b6307ad5d
chanmin97 <cmbear314@naver.com>
2024-05-13 17:30:06 +0900

    update

--- hoshinohome/src/main/java/com/ssafy/hoshinohome/model/dto/UserInfo.java
+++ hoshinohome/src/main/java/com/ssafy/hoshinohome/model/dto/UserInfo.java
@@ -4,26 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class UserInfo {
-    private String user_id; // PK
-    private String user_pwd;
+    private int user_id; // PK
     private String user_name;
+    private String user_password;
     private String user_address;
-    private String user_phone;
-    private String[] user_favorite_place; // HOW?
+    private String user_favorite_place;
+    private String user_type;
 
     public UserInfo() {
     }
 
-    public UserInfo(String user_id, String user_pwd, String user_name, String user_address, String user_phone,
-                    String[] user_favorite_place) {
+    public UserInfo(int user_id, String user_name, String user_password, String user_address, String user_favorite_place, String user_type) {
         this.user_id = user_id;
-        this.user_pwd = user_pwd;
         this.user_name = user_name;
+        this.user_password = user_password;
         this.user_address = user_address;
-        this.user_phone = user_phone;
         this.user_favorite_place = user_favorite_place;
+        this.user_type = user_type;
     }
-
 }
