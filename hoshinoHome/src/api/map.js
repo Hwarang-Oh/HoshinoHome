09196a61743dd8d25a1bc2173c86e965f4e8d839
chanmin97 <cmbear314@naver.com>
2024-05-13 14:48:20 +0900

    update

--- hoshinoHome/src/api/map.js
+++ hoshinoHome/src/api/map.js
@@ -3,7 +3,7 @@ import { noticeAxios } from '@/utils/axios'
 const mapAPI = noticeAxios()
 
 const getDealList = (range, success, fail) => {
-  mapAPI.post('api/map/list', range).then(success).then(fail)
+  mapAPI.post('api/map/list', range).then(success).catch(fail)
 }
 
 export default { getDealList }
