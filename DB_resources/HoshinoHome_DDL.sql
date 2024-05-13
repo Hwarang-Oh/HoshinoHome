48d258628428b43f6dc2136a46b5d7396e7afa51
chanmin97 <cmbear314@naver.com>
2024-05-13 17:32:11 +0900

    up

--- DB_resources/HoshinoHome_DDL.sql
+++ DB_resources/HoshinoHome_DDL.sql
@@ -1,4 +1,5 @@
 -- Active: 1710890946343@@127.0.0.1@3306@hoshinohome''
+-- drop schema hoshinohome; 
 CREATE SCHEMA IF NOT EXISTS `hoshinoHome` DEFAULT CHARACTER SET utf8;
 
 use hoshinohome;
@@ -10,7 +11,7 @@ use hoshinohome;
 DROP TABLE IF EXISTS `dong`;
 
 CREATE TABLE `dong` (
-    `dong_code` varchar(10) NOT NULL, `si_do_name` varchar(30) DEFAULT NULL, `gu_gun_Name` varchar(30) DEFAULT NULL, `dong_name` varchar(30) DEFAULT NULL, PRIMARY KEY (`dong_code`)
+    `dong_code` varchar(10) NOT NULL, `si_do_name` varchar(30) DEFAULT NULL, `gu_gun_name` varchar(30) DEFAULT NULL, `dong_name` varchar(30) DEFAULT NULL, PRIMARY KEY (`dong_code`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
@@ -50,7 +51,13 @@ CREATE TABLE `Enviroment` (
 DROP TABLE IF EXISTS `UserInfo`;
 
 CREATE TABLE `UserInfo` (
-    `user_id` VARCHAR(45) NOT NULL, `user_pwd` VARCHAR(45) NULL, `user_name` VARCHAR(45) NULL, `user_address` VARCHAR(45) NULL, `user_phone` VARCHAR(45) NULL, `user_favorite_place` VARCHAR(45) NULL, PRIMARY KEY (`user_id`)
+    `user_id` int NOT NULL AUTO_INCREMENT,
+    `user_name` varchar(50) NOT NULL,
+    `user_password` varchar(100) NOT NULL,
+    `user_address` varchar(100) NOT NULL,
+    `user_favorite_place` varchar(100) NOT NULL,
+    `user_type` varchar(10) NOT NULL,
+    PRIMARY KEY (`user_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
@@ -68,7 +75,7 @@ CREATE TABLE `Notice` (
 DROP TABLE IF EXISTS `DongStory`;
 
 CREATE TABLE `DongStory` (
-    `post_id` INT NOT NULL AUTO_INCREMENT, `dong_code` varchar(10) NOT NULL, `title` VARCHAR(45) NULL, `content` VARCHAR(45) NULL, `date` DATETIME NULL, `user_id` VARCHAR(45) NOT NULL, PRIMARY KEY (`post_id`), KEY `DongStory_dong_code_dong_dong_code_fk_idx` (`dong_code`), CONSTRAINT `DongStory_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`), KEY `DongStory_user_id_userinfo_user_id_fk_idx` (`user_id`), CONSTRAINT `DongStory_user_id_userinfo_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `userinfo` (`user_id`)
+    `post_id` INT NOT NULL AUTO_INCREMENT, `dong_code` varchar(10) NOT NULL, `title` VARCHAR(45) NULL, `content` VARCHAR(45) NULL, `date` DATETIME NULL, `user_id` int NOT NULL, PRIMARY KEY (`post_id`), KEY `DongStory_dong_code_dong_dong_code_fk_idx` (`dong_code`), CONSTRAINT `DongStory_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`), KEY `DongStory_user_id_userinfo_user_id_fk_idx` (`user_id`), CONSTRAINT `DongStory_user_id_userinfo_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `userinfo` (`user_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
@@ -77,5 +84,5 @@ CREATE TABLE `DongStory` (
 DROP TABLE IF EXISTS `Comment`;
 
 CREATE TABLE `Comment` (
-    `comment_id` INT NOT NULL AUTO_INCREMENT, `post_id` INT NOT NULL, `user_id` VARCHAR(45) NOT NULL, `content` VARCHAR(45) NULL, `date` DATETIME NULL, PRIMARY KEY (`comment_id`), KEY `Comment_post_id_DongStory_post_id_fk_idx` (`post_id`), CONSTRAINT `Comment_post_id_DongStory_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `DongStory` (`post_id`), KEY `Comment_user_id_userinfo_user_id_fk_idx` (`user_id`), CONSTRAINT `Comment_user_id_userinfo_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `userinfo` (`user_id`)
+    `comment_id` INT NOT NULL AUTO_INCREMENT, `post_id` INT NOT NULL, `user_id` int NOT NULL, `content` VARCHAR(45) NULL, `date` DATETIME NULL, PRIMARY KEY (`comment_id`), KEY `Comment_post_id_DongStory_post_id_fk_idx` (`post_id`), CONSTRAINT `Comment_post_id_DongStory_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `DongStory` (`post_id`), KEY `Comment_user_id_userinfo_user_id_fk_idx` (`user_id`), CONSTRAINT `Comment_user_id_userinfo_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `userinfo` (`user_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
