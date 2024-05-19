ac5557bb8d1ccf0e4807a6db037e96c74b27b373
ChanminLee <cmbear314@naver.com>
2024-05-20 00:12:57 +0900

    Add: DongStory 백엔드 임시저장

--- hoshinohome/src/main/java/com/ssafy/hoshinohome/model/service/DongStoryService.java
+++ hoshinohome/src/main/java/com/ssafy/hoshinohome/model/service/DongStoryService.java
@@ -1,5 +1,12 @@
 package com.ssafy.hoshinohome.model.service;
 
-public interface DongStoryService {
+import com.ssafy.hoshinohome.model.dto.DongStory;
+import java.util.List;
 
+public interface DongStoryService {
+    boolean addDongStory(DongStory dongStory) throws Exception;
+    boolean modifyDongStory(DongStory dongStory) throws Exception;
+    boolean removeDongStory(int post_id) throws Exception;
+    DongStory getDongStory(int post_id) throws Exception;
+    List<DongStory> getDongStoriesByRegion(String region) throws Exception;
 }
