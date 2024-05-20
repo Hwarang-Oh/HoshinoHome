5b1c33d02add1be27fc143737958dde640458b1f
Hwarang <keyein123@gmail.com>
2024-05-20 14:56:46 +0900

    240520_DB_Resources Update

--- DB_resources/HoshinoHome_DDL.sql
+++ DB_resources/HoshinoHome_DDL.sql
@@ -1,8 +1,6 @@
--- Active: 1710890946343@@127.0.0.1@3306@hoshinohome''
--- drop schema hoshinohome; 
 CREATE SCHEMA IF NOT EXISTS `hoshinoHome` DEFAULT CHARACTER SET utf8;
 
-use hoshinohome;
+USE hoshinoHome;
 
 -- -----------------------------------------------------
 -- Table `hoshinoHome`.`dong`
@@ -11,7 +9,11 @@ use hoshinohome;
 DROP TABLE IF EXISTS `dong`;
 
 CREATE TABLE `dong` (
-    `dong_code` varchar(10) NOT NULL, `si_do_name` varchar(30) DEFAULT NULL, `gu_gun_name` varchar(30) DEFAULT NULL, `dong_name` varchar(30) DEFAULT NULL, PRIMARY KEY (`dong_code`)
+    `dong_code` varchar(10) NOT NULL,
+    `si_do_name` varchar(30) DEFAULT NULL,
+    `gu_gun_name` varchar(30) DEFAULT NULL,
+    `dong_name` varchar(30) DEFAULT NULL,
+    PRIMARY KEY (`dong_code`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
@@ -21,9 +23,27 @@ CREATE TABLE `dong` (
 DROP TABLE IF EXISTS `houseinfo`;
 
 CREATE TABLE `houseinfo` (
-    `apt_code` bigint NOT NULL, `build_year` int DEFAULT NULL, `road_name` varchar(40) DEFAULT NULL, `road_name_bonbun` varchar(5) DEFAULT NULL, `road_name_bubun` varchar(5) DEFAULT NULL, `road_name_seq` varchar(2) DEFAULT NULL, `road_name_basement_code` varchar(1) DEFAULT NULL, `road_name_code` varchar(7) DEFAULT NULL, `dong` varchar(40) DEFAULT NULL, `bonbun` varchar(4) DEFAULT NULL, `bubun` varchar(4) DEFAULT NULL, `si_gun_gu_code` varchar(5) DEFAULT NULL, `eub_myun_dong_code` varchar(5) DEFAULT NULL, `dong_code` varchar(10) DEFAULT NULL, `land_code` varchar(1) DEFAULT NULL, `apartment_name` varchar(40) DEFAULT NULL, `jibun` varchar(10) DEFAULT NULL, `lng` varchar(30) DEFAULT NULL, `lat` varchar(30) DEFAULT NULL, PRIMARY KEY (`apt_code`), UNIQUE KEY `UNIQUE` (
-        `build_year`, `apartment_name`, `jibun`, `si_gun_gu_code`, `eub_myun_dong_code`
-    ) /*!80000 INVISIBLE */, KEY `houseinfo_dong_code_dong_dong_code_fk_idx` (`dong_code`) /*!80000 INVISIBLE */, CONSTRAINT `houseinfo_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`)
+    `house_code` bigint NOT NULL,
+    `house_type` int NOT NULL,
+    `build_year` int DEFAULT NULL,
+    `dong_code` varchar(10) DEFAULT NULL,
+    `house_name` varchar(30) DEFAULT NULL,
+    `si_gun_gu_name` varchar(20) DEFAULT NULL,
+    `dong_name` varchar(20) DEFAULT NULL,
+    `road_name` varchar(20) DEFAULT NULL,
+    `bonbun` varchar(5) DEFAULT NULL,
+    `bubun` varchar(5) DEFAULT NULL,
+    `jibun_address` varchar(40) DEFAULT NULL,
+    `road_address` VARCHAR(40) DEFAULT NULL,
+    `lng` varchar(30) DEFAULT NULL,
+    `lat` varchar(30) DEFAULT NULL,
+    PRIMARY KEY (`house_code`),
+    UNIQUE KEY `UNIQUE` (
+        `jibun_address`,
+        `road_address`
+    ),
+    KEY `houseinfo_dong_code_dong_dong_code_fk_idx` (`dong_code`),
+    CONSTRAINT `houseinfo_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
@@ -33,21 +53,47 @@ CREATE TABLE `houseinfo` (
 DROP TABLE IF EXISTS `housedeal`;
 
 CREATE TABLE `housedeal` (
-    `deal_code` bigint NOT NULL, `deal_amount` varchar(20) DEFAULT NULL, `deal_year` int DEFAULT NULL, `deal_month` int DEFAULT NULL, `deal_day` int DEFAULT NULL, `area` varchar(20) DEFAULT NULL, `floor` varchar(4) DEFAULT NULL, `cancel_deal_type` varchar(1) DEFAULT NULL, `apt_code` bigint DEFAULT NULL, PRIMARY KEY (`deal_code`), KEY `housedeal_apt_code_houseinfo_apt_code_fk_idx` (`apt_code`), CONSTRAINT `housedeal_apt_code_houseinfo_apt_code_fk` FOREIGN KEY (`apt_code`) REFERENCES `houseinfo` (`apt_code`)
+    `deal_code` bigint NOT NULL,
+    `deal_type` INT NOT NULL,
+    `deal_amount` varchar(20) DEFAULT NULL,
+    `deposit_amount` varchar(20) DEFAULT NULL,
+    `monthly_amount` varchar(20) DEFAULT NULL,
+    `jibun_address` varchar(40) DEFAULT NULL,
+    `road_address` varchar(40) DEFAULT NULL,
+    `deal_year` int DEFAULT NULL,
+    `deal_month` int DEFAULT NULL,
+    `deal_day` int DEFAULT NULL,
+    `area` varchar(20) DEFAULT NULL,
+    `floor` varchar(4) DEFAULT NULL,
+    `house_code` bigint DEFAULT NULL,
+    `house_type` int DEFAULT NULL,
+    PRIMARY KEY (`deal_code`),
+    KEY `housedeal_house_code_houseinfo_house_code_fk_idx` (`house_code`),
+    CONSTRAINT `housedeal_house_code_houseinfo_house_code_fk` FOREIGN KEY (`house_code`) REFERENCES `houseinfo` (`house_code`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table `hoshinoHome`.`Enviroment`
 -- -----------------------------------------------------
+
 DROP TABLE IF EXISTS `Enviroment`;
 
 CREATE TABLE `Enviroment` (
-    `dong_code` varchar(10) NOT NULL, `station_proximity` INT NULL, `commercial_area` INT NULL, `Academic_num` INT NULL, `Security_Index` INT NULL, `dong_score` INT NULL, PRIMARY KEY (`dong_code`), KEY `Enviroment_dong_code_dong_dong_code_fk_idx` (`dong_code`), CONSTRAINT `Enviroment_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`)
+    `dong_code` varchar(10) NOT NULL,
+    `station_proximity` INT NULL,
+    `commercial_area` INT NULL,
+    `Academic_num` INT NULL,
+    `Security_Index` INT NULL,
+    `dong_score` INT NULL,
+    PRIMARY KEY (`dong_code`),
+    KEY `Enviroment_dong_code_dong_dong_code_fk_idx` (`dong_code`),
+    CONSTRAINT `Enviroment_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table `hoshinoHome`.`UserInfo`
 -- -----------------------------------------------------
+
 DROP TABLE IF EXISTS `UserInfo`;
 
 CREATE TABLE `UserInfo` (
@@ -63,19 +109,35 @@ CREATE TABLE `UserInfo` (
 -- -----------------------------------------------------
 -- Table `hoshinoHome`.`Notice`
 -- -----------------------------------------------------
+
 DROP TABLE IF EXISTS `Notice`;
 
 CREATE TABLE `Notice` (
-    `post_id` INT NOT NULL AUTO_INCREMENT, `title` VARCHAR(45) NULL, `content` VARCHAR(45) NULL, `date` DATETIME NULL, PRIMARY KEY (`post_id`)
+    `post_id` INT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(45) NULL,
+    `content` VARCHAR(45) NULL,
+    `date` DATETIME NULL,
+    PRIMARY KEY (`post_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table `hoshinoHome`.`DongStory`
 -- -----------------------------------------------------
+
 DROP TABLE IF EXISTS `DongStory`;
 
 CREATE TABLE `DongStory` (
-    `post_id` INT NOT NULL AUTO_INCREMENT, `dong_code` varchar(10) NOT NULL, `title` VARCHAR(45) NULL, `content` VARCHAR(45) NULL, `date` DATETIME NULL, `user_id` int NOT NULL, PRIMARY KEY (`post_id`), KEY `DongStory_dong_code_dong_dong_code_fk_idx` (`dong_code`), CONSTRAINT `DongStory_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`), KEY `DongStory_user_id_userinfo_user_id_fk_idx` (`user_id`), CONSTRAINT `DongStory_user_id_userinfo_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `userinfo` (`user_id`)
+    `post_id` INT NOT NULL AUTO_INCREMENT,
+    `dong_code` varchar(10) NOT NULL,
+    `title` VARCHAR(45) NULL,
+    `content` VARCHAR(45) NULL,
+    `date` DATETIME NULL,
+    `user_id` int NOT NULL,
+    PRIMARY KEY (`post_id`),
+    KEY `DongStory_dong_code_dong_dong_code_fk_idx` (`dong_code`),
+    CONSTRAINT `DongStory_dong_code_dong_dong_code_fk` FOREIGN KEY (`dong_code`) REFERENCES `dong` (`dong_code`),
+    KEY `DongStory_user_id_userinfo_user_id_fk_idx` (`user_id`),
+    CONSTRAINT `DongStory_user_id_userinfo_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `UserInfo` (`user_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
@@ -83,6 +145,13 @@ CREATE TABLE `DongStory` (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Comment`;
 
-CREATE TABLE `Comment` (
-    `comment_id` INT NOT NULL AUTO_INCREMENT, `post_id` INT NOT NULL, `user_id` int NOT NULL, `content` VARCHAR(45) NULL, `date` DATETIME NULL, PRIMARY KEY (`comment_id`), KEY `Comment_post_id_DongStory_post_id_fk_idx` (`post_id`), CONSTRAINT `Comment_post_id_DongStory_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `DongStory` (`post_id`), KEY `Comment_user_id_userinfo_user_id_fk_idx` (`user_id`), CONSTRAINT `Comment_user_id_userinfo_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `userinfo` (`user_id`)
+CREATE TABLE Comment (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT,
+    board_type VARCHAR(50),
+    user_id INT,
+    user_name VARCHAR(50),
+    content VARCHAR(255),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES UserInfo (user_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
